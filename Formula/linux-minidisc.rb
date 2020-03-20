b7422e368652c36d8dfcba2366123e285e122ea8
Jessica Stokes <hello@jessicastokes.net>
2020-03-19 21:18:53 -0700

    Update to my linux-minidisc fork which decodes Shift JIS properly!

--- Formula/linux-minidisc.rb
+++ Formula/linux-minidisc.rb
@@ -1,10 +1,10 @@
 class LinuxMinidisc < Formula
   desc "Free software for accessing MiniDisc devices"
-  homepage "https://github.com/glaubitz/linux-minidisc"
-  url "https://github.com/glaubitz/linux-minidisc.git", :tag => "0.9.16"
-  version "0.9.16"
+  homepage "https://github.com/ticky/linux-minidisc"
+  url "https://github.com/ticky/linux-minidisc.git", :tag => "0.9.17-ticky.1"
+  version "0.9.17-ticky.1"
 
-  head "https://github.com/glaubitz/linux-minidisc.git"
+  head "https://github.com/ticky/linux-minidisc.git"
 
   depends_on "pkg-config" => :build
 
