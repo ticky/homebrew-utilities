6264028dd33ab43fe4a508b7022c6216e833cf4b
Jessica Stokes <hello@jessicastokes.net>
2020-07-26 21:38:35 -0700

    Add cxmon, "A Command-Line File Manipulation Tool And Disassembler"
    It turned out it wasn't the program I wanted, but I got it building so here it is.

---
+++ Formula/cxmon.rb
@@ -0,0 +1,21 @@
+class Cxmon < Formula
+  desc "A Command-Line File Manipulation Tool And Disassembler"
+  homepage "http://cxmon.cebix.net/#download"
+  url "https://github.com/cebix/macemu.git", :revision => "89bcd3dc3ed663934865f7e9e1b99c13a1c13db7"
+  version "2020-07-05T21:56:44Z"
+
+  head "https://github.com/cebix/macemu.git"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+
+  def install
+    cd "cxmon" do
+      system "./bootstrap"
+      system "./configure"
+      system "make"
+      bin.install "src/cxmon"
+      man1.install "cxmon.1"
+    end
+  end
+end
