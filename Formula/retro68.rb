056f8c9da1a9150614634f736cb8bb7f17184478
Jessica Stokes <hello@jessicastokes.net>
2020-07-30 23:04:04 -0700

    Add retro68 (https://github.com/autc04/Retro68) cross-compiler

---
+++ Formula/retro68.rb
@@ -0,0 +1,83 @@
+class Retro68 < Formula
+  desc "GCC-based cross-compiler for classic 68K and PPC Macintoshes"
+  homepage "https://github.com/autc04/Retro68/"
+  url "https://github.com/autc04/Retro68.git", commit: "fbdf2c4bcbeed434ab803a5899792612204074bf"
+  version "2020-07-22T180229Z"
+  head "https://github.com/autc04/Retro68.git"
+  # Formula adapted from https://github.com/Homebrew/homebrew-core/pull/43442
+
+  conflicts_with "binutils",
+                 :because => "both install a `share/info/bfd.info` file"
+
+  depends_on "boost"
+  depends_on "cmake"
+  depends_on "gmp"
+  depends_on "mpfr"
+  depends_on "libmpc"
+
+  # Note: brew's robot said non-system bison might not be necessary, but I built with it anyway, YMMV
+  depends_on "bison"
+
+  resource "mpw" do
+    url "https://staticky.com/mirrors/ftp.apple.com/developer/Tool_Chest/Core_Mac_OS_Tools/MPW_etc./MPW-GM_Images/MPW-GM.img.bin"
+    sha256 "99bbfa95bb9800c8ffc572fce6d72e561f012331c5c623fa45f732502b6fa872"
+  end
+
+  def download_interfaces(dst)
+  end
+
+  def install
+    tmpdir = Pathname.new(Dir.mktmpdir)
+    tmpdir.install resource("mpw")
+
+    chdir tmpdir do
+      system "macbinary", "decode", "MPW-GM.img.bin"
+      system "hdiutil", "convert", "MPW-GM.img",
+                        "-format", "UDRO", "-o", "MPW-GM"
+      system "hdiutil", "attach", "MPW-GM.dmg"
+    end
+
+    src = "/Volumes/MPW-GM/MPW-GM/Interfaces&Libraries/"
+    dst = "InterfacesAndLibraries/"
+    cp_r src + "Interfaces", dst
+
+    # cp_r does not copy resource forks.
+    # rubocop:disable FormulaAudit/Miscellaneous
+    system "cp", "-r", src + "Libraries", dst
+    # rubocop:enable FormulaAudit/Miscellaneous
+    system "hdiutil", "detach", "/Volumes/MPW-GM"
+
+    mkdir "build" do
+      system "../build-toolchain.bash", "--prefix=#{prefix}"
+    end
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write "add_application(Test test.c CONSOLE)"
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      int main(int argc, char** argv)
+      {
+        printf("Hello");
+        return 0;
+      }
+    EOS
+    cmake_files = [
+      "#{prefix}/m68k-apple-macos/cmake/retro68.toolchain.cmake",
+      "#{prefix}/powerpc-apple-macos/cmake/retrocarbon.toolchain.cmake",
+      "#{prefix}/powerpc-apple-macos/cmake/retroppc.toolchain.cmake",
+    ]
+    cmake_files.each do |cmake_file|
+      mkdir "build" do
+        ENV["CFLAGS"] = nil
+        ENV["CPATH"] = nil
+        ENV["CPPFLAGS"] = nil
+        ENV["CXXFLAGS"] = nil
+        ENV["LDFLAGS"] = nil
+        system "cmake", "..", "-DCMAKE_TOOLCHAIN_FILE=#{cmake_file}"
+        system "make"
+      end
+      rm_rf "build"
+    end
+  end
+end
