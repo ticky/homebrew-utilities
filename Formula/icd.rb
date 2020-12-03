2c06884bdb153964584e368d767d6559bb634fbc
Jessica Stokes <hello@jessicastokes.net>
2020-12-02 20:12:05 -0800

    Bump icd version
    Closes #5.
    
    Signed-off-by: Jessica Stokes <282113+ticky@users.noreply.github.com>

--- Formula/icd.rb
+++ Formula/icd.rb
@@ -1,7 +1,8 @@
 class Icd < Formula
   desc "Simple CLI interface for fetching file and folder from the iCloud Storage"
   homepage "https://github.com/farnots/iCloudDownloader"
-  url "https://github.com/farnots/iCloudDownloader.git", tag: "v1.0"
+  url "https://github.com/farnots/iCloudDownloader.git", revision: "74491c772500161079feccf3d7b5ba367fcf4420"
+  version "1.0-ticky.1"
   head "https://github.com/farnots/iCloudDownloader.git"
 
   depends_on :macos
