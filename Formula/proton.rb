76f3a762fc0216bc7bd2cd3e503112579c812ab2
Jessica Stokes <hello@jessicastokes.net>
2020-12-02 20:40:22 -0800

    WIP Proton formula

---
+++ Formula/proton.rb
@@ -0,0 +1,19 @@
+class Proton < Formula
+  desc "Compatibility tool for Steam Play based on Wine and additional components"
+  homepage "https://github.com/ValveSoftware/Proton"
+  url "https://github.com/ValveSoftware/Proton.git", :tag => "proton-3.7-20180821"
+  version "3.7-20180821"
+
+  head "https://github.com/ValveSoftware/Proton.git"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "nasm" => :build
+  depends_on "cmake" => :build
+
+  def install
+    system "./build_proton.sh"
+    system "ls", "dist"
+  end
+end
