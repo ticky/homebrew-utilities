60d2d7dfe6ac7eeb800265df86a768cbf8c5dac8
Jessica Stokes <hello@jessicastokes.net>
2020-12-02 20:43:29 -0800

    Bump revision on tcat
    Closes #16.
    
    Signed-off-by: Jessica Stokes <282113+ticky@users.noreply.github.com>

--- Formula/tcat.rb
+++ Formula/tcat.rb
@@ -3,6 +3,7 @@ class Tcat < Formula
   homepage "https://github.com/ticky/tcat"
   url "https://github.com/ticky/tcat.git", revision: "ddf7dc3cb3e902216f65e47d4daf02ff0628e20f"
   version "2014-08-24T511029Z"
+  revision 1
 
   head "https://github.com/ticky/tcat.git"
 
