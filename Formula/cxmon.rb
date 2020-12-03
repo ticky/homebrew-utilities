ea336ead1407c0c241b9d7d8518d66af3895387b
Jessica Stokes <282113+ticky@users.noreply.github.com>
2020-12-03 05:29:48 +0000

    cxmon: add 2020-07-05T515644Z_1 bottle.

--- Formula/cxmon.rb
+++ Formula/cxmon.rb
@@ -7,6 +7,12 @@ class Cxmon < Formula
 
   head "https://github.com/cebix/macemu.git"
 
+  bottle do
+    root_url "https://github.com/ticky/homebrew-utilities/releases/download/cxmon-2020-07-05T515644Z_1"
+    cellar :any_skip_relocation
+    sha256 "1002859f4738ee9b1eb76fac1f845c1cd3a857ae0b63506ed95fa141e4272c0e" => :catalina
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on :macos
