f5261559de3d73d03a63b5034d45dabd0e156bcf
Jessica Stokes <hello@jessicastokes.net>
2020-12-02 20:30:30 -0800

    Bump airchat revision

--- Formula/airchat.rb
+++ Formula/airchat.rb
@@ -3,6 +3,7 @@ class Airchat < Formula
   homepage "https://github.com/chendo/airchat"
   url "https://github.com/chendo/airchat.git", revision: "d0da3bec0d94b88bf8a990bbebbd7703ced5fc12"
   version "2016-12-11T354211Z"
+  revision 1
 
   head "https://github.com/chendo/airchat.git"
 
