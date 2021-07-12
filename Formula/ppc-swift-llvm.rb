6de89111d6e75df5fece386023c6120c38342e65
Jessica Stokes <hello@jessicastokes.net>
2021-07-12 11:48:09 -0700

    Fix not using git URL

--- Formula/ppc-swift-llvm.rb
+++ Formula/ppc-swift-llvm.rb
@@ -1,10 +1,10 @@
 class PpcSwiftLlvm < Formula
   desc "Macintosh Programmer's Workshop (mpw) compatibility layer"
   homepage "https://belkadan.com/source/ppc-swift-project"
-  url "https://belkadan.com/source/ppc-swift-project", revision: "2fabc10dfcba0eb0ac8957b749b68da1593cece6"
+  url "https://belkadan.com/source/ppc-swift-project", using: :git, revision: "2fabc10dfcba0eb0ac8957b749b68da1593cece6"
   version "2020-07-12"
 
-  head "https://belkadan.com/source/ppc-swift-project"
+  head "https://belkadan.com/source/ppc-swift-project", using: :git
 
   depends_on "cmake" => :build
   depends_on "ninja" => :build
