aa0539eac414496bebd385857f9c46f0d02553d9
Jessica Stokes <hello@jessicastokes.net>
2021-07-11 20:29:25 -0700

    Add Aaru, a media dump management and conversion utility

---
+++ Casks/aaru.rb
@@ -0,0 +1,21 @@
+cask "aaru" do
+  version "5.2.0.3330"
+  sha256 "e6c2c777c231195eb227b93b3149a393bb560a9c535a456d9335226d98255b22"
+
+  url "https://github.com/aaru-dps/Aaru/releases/download/v#{version}/aaru-#{version}-1_macos.zip"
+  name "Aaru"
+  desc "Fully featured media dump management solution"
+  homepage "https://github.com/aaru-dps/Aaru"
+
+  binary "aaru"
+
+  zap trash: [
+    "~/Library/Preferences/com.claunia.aaru.plist",
+  ]
+
+  def caveats
+    <<~EOS
+      Due to how dependencies are loaded, Aaru may fail to run unless installed with the `--no-quarantine` flag set.
+    EOS
+  end
+end
