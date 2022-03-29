f7904f7fc8d7b452edbbafebae8f1c7f81b70c41
Jessica Stokes <282113+ticky@users.noreply.github.com>
2022-03-29 04:39:39 +0000

    space-cadet-pinball: add 2.0.1 bottle.

--- Formula/space-cadet-pinball.rb
+++ Formula/space-cadet-pinball.rb
@@ -4,6 +4,12 @@ class SpaceCadetPinball < Formula
   url "https://github.com/k4zmu2a/SpaceCadetPinball.git", tag: "Release_2.0.1"
   head "https://github.com/k4zmu2a/SpaceCadetPinball.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/ticky/homebrew-utilities/releases/download/space-cadet-pinball-2.0.1"
+    sha256 cellar: :any,                 big_sur:      "d40aaa3d91d06730fbb72b9f915cf6ce9d185e93d333db398855c72b05827e42"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "3e96aea3f9943aa8b08b5519b50002fce5193324476955b146badc353157c171"
+  end
+
   depends_on "cmake" => :build
   depends_on "sdl2"
   depends_on "sdl2_mixer"
