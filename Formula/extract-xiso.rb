26547c2b444c440af9c6f1949b33ca023bf21925
Jessica Stokes <hello@jessicastokes.net>
2022-04-27 22:24:37 -0700

    extract-xiso 2.7.1-ticky.1 (new formula)
    * Add extract-xiso formula
    * Correct license
    * Correct URLs
    
    Closes #33.
    
    Signed-off-by: Jessica Stokes <282113+ticky@users.noreply.github.com>

---
+++ Formula/extract-xiso.rb
@@ -0,0 +1,21 @@
+class ExtractXiso < Formula
+  desc "Xbox ISO Creation/Extraction utility"
+  homepage "https://sourceforge.net/projects/extract-xiso/"
+  url "https://github.com/XboxDev/extract-xiso.git", revision: "4488c39d7aa0bd0c371929a3fdeb456123aa46b3"
+  version "2.7.1-ticky.1"
+  license "BSD-4-Clause"
+  head "https://github.com/XboxDev/extract-xiso.git", branch: "master"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    assert_match "extract-xiso v#{version.to_s.split("-").first}",
+                 shell_output("#{bin}/extract-xiso -v")
+  end
+end
