1439969ef9705ff093793e9f579311ace0641b23
Jessica Stokes <hello@jessicastokes.net>
2022-04-27 22:34:57 -0700

    Correct URLs

--- Formula/extract-xiso.rb
+++ Formula/extract-xiso.rb
@@ -1,10 +1,10 @@
 class ExtractXiso < Formula
   desc "Xbox ISO Creation/Extraction utility"
-  homepage "http://sourceforge.net/projects/extract-xiso/"
+  homepage "https://sourceforge.net/projects/extract-xiso/"
   url "https://github.com/XboxDev/extract-xiso.git", revision: "4488c39d7aa0bd0c371929a3fdeb456123aa46b3"
   version "2.7.1-ticky.1"
   license "BSD-4-Clause"
-  head "https://github.com/XboxDev/extract-xiso.git"
+  head "https://github.com/XboxDev/extract-xiso.git", branch: "master"
 
   depends_on "cmake" => :build
 
