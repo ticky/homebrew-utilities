ac2747659e2f1868c64dad07b5e618e50e2ff97b
Jessica Stokes <hello@jessicastokes.net>
2024-05-26 14:47:33 -0700

    pcem: fix version

--- Formula/pcem.rb
+++ Formula/pcem.rb
@@ -2,7 +2,7 @@ class Pcem < Formula
   desc "IBM PC emulator"
   homepage "https://pcem-emulator.co.uk"
   url "https://github.com/PCemOnMac/PCemV17macOS.git", revision: "180d7cd7f924219534813c5b38987834bb9971df"
-  version "v17.0.0-beta+git180d7cd7f924219534813c5b38987834bb9971df"
+  version "17.0.0-beta+git180d7cd7f924219534813c5b38987834bb9971df"
 
   head "https://github.com/PCemOnMac/PCemV17macOS.git"
 
